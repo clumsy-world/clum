2d0594b4a32f6e352db8746d827be054f0a67514
hideyuki-hori <montecarlo1101@gmail.com>
2025-07-28 06:50:26 +0900

    first step

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct ClumParser;
+
 fn main() {
-    println!("Hello, world!");
+    let code = "data |> process |> save |> !";
+    
+    match ClumParser::parse(Rule::program, code) {
+        Ok(pairs) => {
+            for pair in pairs {
+                println!("{:#?}", pair);
+            }
+        }
+        Err(e) => println!("{}", e),
+    }
 }
